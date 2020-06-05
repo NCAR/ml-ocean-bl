3116f62aa1446f3a1390a89ee14925a79332d496
Dallas Foster <fostdall@ucar.edu>
2020-06-05 16:30:14 -0600

    changed climatology scheme and added basic plotting functions

--- mloceanbl/data.py
+++ mloceanbl/data.py
@@ -12,9 +12,6 @@
 # Then the file saves the new data set to file at sss_sst_ssh_weekly_halfdegree.nc'
 
 ## TO DO
-# Potentially update interpolation method from linear interpolant to gaussian process
-# rethink and be more careful with temporal interpolation of 5 day ssh averaging. 
-
 # add units and metadata to new array
 # add section for mldb data
 
@@ -23,36 +20,72 @@
 ####################################################################################
 
 import xarray as xr
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from netCDF4 import Dataset
 from glob import glob
-import scipy as sp
+from scipy.interpolate import interpn
+from mpl_toolkits.basemap import maskoceans
 
 ####################################################################################
 ############################# Import data ##########################################
 ####################################################################################
+
 print('Beginning to Import Data')
+
+# 1. Import SSS Data from OISSS and SMAP (for climatology)
+# 2. Import SST Data
+# 3. Import SSH Data, transform lon grid to (-180, 180)
+
+############################### Salinity ###### ####################################
+
 ## Import SSS Dataset
-SSS_path = '/glade/work/dwhitt/OISSS/'
-SSS_files = sorted(glob(SSS_path+'*.nc'))
 SSS_path = '/glade/work/dwhitt/OISSS/'
 SSS_files = sorted(glob(SSS_path+'*.nc'))
 sss = xr.open_mfdataset(SSS_files, decode_times=False, combine='by_coords')
 
 # Convert to time variable to dateTime range
-sss = sss.assign_coords(time = pd.date_range('2011-08-26T12:00:00', periods=200, freq = 'W-FRI'))
+sss = sss.assign_coords(time = pd.date_range(
+                        '2011-08-26T12:00:00', periods=200, freq = 'W-FRI'))
 
 # Rename coordinate names for unity
 sss = sss.rename({'longitude':'lon', 'latitude':'lat'})
 
+# Create Mask
+sss_lat_mask = (sss.lat>-55) & (sss.lat < 60)
+
+## Import SSS data 2015-2020 to calculate climatologies
+SSS_clim_path = '/glade/scratch/dwhitt/SMAP_RSS_L3_8d/'
+SSS_clim_files = sorted(glob(SSS_clim_path+'*.nc'))
+with xr.open_mfdataset(SSS_clim_files, combine='by_coords') as sss_clim:
+    # Change from (0, 360) -> (-180, 180)
+    sss_clim = sss_clim.assign_coords(lon=(((sss_clim.lon + 180) % 360) - 180)).sortby('lon')
+    
+    # Get on to sss 1/2 degree lat lon grid, select 7/2015 - 2020
+    sss_climatology = sss_clim.sss_smap[81:].interp(lat = sss.lat, lon = sss.lon)
+    
+    # Take monthly rolling average
+    sss_climatology = sss_climatology.chunk((45, 720,1440)).rolling(
+                        time=28,center=True).reduce(np.nanmean)
+    
+    # Group by day of year and take average over 4 years
+    sss_climatology = sss_climatology[28:-28].groupby('time.dayofyear').mean('time')
+    
+    # Rechunk
+    sss_climatology = sss_climatology.chunk((1,sss.lat.size, sss.lon.size))
+    
+
+########################### Sea Level Temperatures ####################################
 
 ## Import SST Dataset
 SST_path = '/glade/work/dwhitt/GHRSST_OIMW/'
 SST_files = sorted(glob(SST_path+'*.nc'))
 sst = xr.open_mfdataset(SST_files, combine='by_coords')
 
+# Create Mask
+sst_lat_mask = (sst.lat>-55) & (sst.lat < 60)
+
+############################### Sea Level Height ####################################
 
 ## Import SSH Dataset
 SSH_path = '/glade/scratch/dwhitt/measures_ssh/'
@@ -72,31 +105,35 @@ ssh = ssh.assign_coords(lon=(((ssh.lon + 180) % 360) - 180)).sortby('lon')
 ############################# Interpolate Data #####################################
 ####################################################################################
 print('Beginning to Interpolate Data')
-## Create masks for latitude (needed because ssh data is only defined (-79, 79) )
-sst_lat_mask = (sst.lat>-79) & (sst.lat < 79)
-sss_lat_mask = (sss.lat>-79) & (sss.lat < 79)
-sss_nan_mask = ~np.isnan(sss.sss) 
 
-## Convert SST data from daily to weekly
-time_interp_sst = sst.analysed_sst.chunk((7896, 720, 1440)).rolling(time=7).reduce(np.mean).chunk((1,720, 1440)).sel(time = pd.date_range('2011-08-26T12:00:00', periods=200, freq = 'W-FRI'))
+# 1. Interpolate Sea level height onto spatial and temporal grid. Due to difficulties with
+# the netcdf file, this has to be done by conversion to numpy array.
 
-## Convert SST data onto 1/2 degree grid (same as sss data, which is the coarsest available )
-sp_t_inter_sst = time_interp_sst.interp(lon = sss.lon, lat = sss.lat[sss_lat_mask])
-sp_t_inter_sst = sp_t_inter_sst.interpolate_na(dim = 'lon', use_coordinate='lon')
-sp_t_inter_sst = sp_t_inter_sst.interpolate_na(dim='lat', use_coordinate='lat')
-sp_t_inter_sst = sp_t_inter_sst.where(sss_nan_mask)
-print(sp_t_inter_sst)
+# 2. Remove climatology of sss data and interpolate nan values. 
 
+# 3. Remove climatology of sst data and interpolate to spatial and temporal grid.
+
+
+## Create a fine ocean mask for interpolation purposes
+Array = np.ma.zeros((ssh.lat.size,ssh.lon.size))
+Lons,Lats = np.meshgrid(np.array(ssh.lon),np.array(ssh.lat))
+MaskedArray = maskoceans(Lons,Lats,np.array(ssh.SLA[0]),resolution='f',grid=1.25)
+
+############################### Sea Level Height ####################################
 
 ## Convert SSH data
 ## Requires a lot of manual work because of issues with built in xarray interpolation.
 
-# Create necessary date time indexes (found manually, need to encompass entire sss data timeframe + 1 on each end)
+# Create necessary date time indexes (found manually, need to encompass entire sss data timeframe + extra on each end)
 indexes = np.arange(1360, 1680)
 times = ssh.time[indexes]
 
 # Convert ssh data to numpy arrays for scipy interpolation
-sla_array = np.array(ssh.SLA.sel(time=times))
+sla = ssh.SLA.sel(time=times).where(MaskedArray.mask).interpolate_na(dim='lon')
+sla = sla.where(MaskedArray.mask).interpolate_na(dim='lat')
+sla = sla.where(MaskedArray.mask)
+
+sla_array = np.array(sla)
 lat_array = np.array(ssh.lat)
 lon_array = np.array(ssh.lon)
 time_array = np.array(ssh.time.sel(time=times))
@@ -104,42 +141,82 @@ nan_mask = np.isnan(sla_array)
 [TIME, LAT, LON] = np.meshgrid(time_array, lat_array, lon_array, indexing='ij');
 
 # Convert sss data to numpy arrays, this will be the grid to interpolate to
-xlat_array = np.array(sss.lat[(sss.lat>-79) & (sss.lat<79)])
+xlat_array = np.array(sss.lat[sss_lat_mask])
 xlon_array = np.array(sss.lon)
 xtime_array = np.array(sss.time)
 [xTIME, xLAT, xLON] = np.meshgrid(xtime_array, xlat_array, xlon_array, indexing='ij');
-
-# mask lats and lons (see note above)
-# xnan_mask = np.isnan(np.array(sss.sss[:, sss_nan_mask, :]))
-# points = (TIME[nan_mask], LAT[nan_mask], LON[nan_mask])
-# values = sla_array[nan_mask]
 
 # Arrange interpolation grid as required by scipy.interpolate.interpn
 interp_mesh = np.array(np.meshgrid(xtime_array.astype('float'), xlat_array, xlon_array, indexing='ij'))
 interp_points = np.rollaxis(interp_mesh, 0, 4)
 
 # Interpolate
-gridded = sp.interpolate.interpn((time_array.astype('float'), lat_array, lon_array), sla_array, interp_points)
+gridded = interpn((time_array.astype('float'), lat_array, lon_array), sla_array, interp_points)
+ssh_nan_mask = ~np.isnan(gridded)
+
+################################ Salinity #####################################################
+# Remove climatology
+anomalies = (sss.sss.groupby("time.dayofyear") - sss_climatology)
+anomalies = anomalies.drop('dayofyear')
+
+# Interpolate nans in SSS data
+sss_lat_interp = anomalies[:, sss_lat_mask, :].interpolate_na(dim='lat', use_coordinate='lat')
+sss_lon_interp = anomalies[:, sss_lat_mask, :].interpolate_na(dim='lon', use_coordinate='lon')
+
+sss_lon_interp = sss_lon_interp.interpolate_na(dim='lat') 
+sss_lat_interp = sss_lat_interp.interpolate_na(dim='lon')
+sss_lat_interp = sss_lat_interp.where(ssh_nan_mask)
+sss_lon_interp = sss_lon_interp.where(ssh_nan_mask)
+sss_interp = 0.5*np.array(sss_lat_interp) + 0.5*np.array(sss_lon_interp)
+
+################################ Temperature #####################################################
+
+# Remove computed climatology from data.
+climatology_mean = sst.analysed_sst.sel(time=pd.date_range('2002-06-01T12:00:00', periods=365.25*8, freq="D").union(
+                    pd.date_range('2016-01-01T12:00:00', periods=366+365+365+180, freq="D"))).chunk((50, 720, 1440))
+
+climatology_mean = climatology_mean.rolling(time=28,center=True).reduce(np.nanmean)
+climatology_mean = climatology_mean[28:-28].groupby('time.dayofyear').mean('time').chunk((1, 720, 1440))
+anomalies = (sst.analysed_sst.sel(time=pd.date_range('2011-01-01T12:00:00', periods = 365+366+365+365+365, freq="D")).groupby("time.dayofyear") - climatology_mean) 
+anomalies = anomalies.drop('dayofyear')
+
+## Convert SST data from daily to weekly
+time_interp_sst = anomalies.chunk((15, 720, 1440)).rolling(time=7).reduce(np.nanmean).chunk((1,720, 1440)).sel(time = pd.date_range('2011-08-26T12:00:00', periods=200, freq = 'W-FRI'))
+
+## Convert SST data onto 1/2 degree grid (same as sss data, which is the coarsest available )
+sp_t_inter_sst = time_interp_sst.interp(lon = sss.lon, lat = sss.lat[sss_lat_mask])
+sp_t_inter_sst = sp_t_inter_sst.interpolate_na(dim = 'lon', use_coordinate='lon')
+sp_t_inter_sst = sp_t_inter_sst.interpolate_na(dim='lat', use_coordinate='lat')
+sp_t_inter_sst = sp_t_inter_sst.where(ssh_nan_mask)
 
 ####################################################################################
 ############################# Combine Data #########################################
 ####################################################################################
 
 ds = xr.Dataset(
-        {'salinity': (['time', 'lat', 'lon'], sss.sss[:, sss_lat_mask, :]), 
+        {'salinity': (['time', 'lat', 'lon'], sss_interp), 
          'temperature': (['time', 'lat', 'lon'], sp_t_inter_sst), 
          'height': (['time', 'lat', 'lon'], gridded)},
         coords={'time': sss.time,
                 'lat': sss.lat[sss_lat_mask],
                 'lon': sss.lon}
 )
-print(ds)
+
+############################## Normalize ###########################################
+
+sal = (ds.salinity - np.nanmean(ds.salinity, axis=0))/np.nanstd(ds.salinity, axis=0)
+ds = ds.assign(norm_salinity = sal)
+temp = (ds.temperature - np.nanmean(ds.temperature, axis=0))/np.nanstd(ds.temperature, axis=0)
+ds = ds.assign(norm_temperature = temp)
+height = (ds.height - np.nanmean(ds.height, axis=0))/np.nanstd(ds.height, axis=0)
+ds = ds.assign(norm_height = height)
 
 ####################################################################################
 ################################ Save Data #########################################
 ####################################################################################
 print('Saving Data')
-ds.to_netcdf(path='sss_sst_ssh_weekly_halfdegree.nc')
+print(ds)
+ds.to_netcdf(path='sss_sst_ssh_normed_anomalies_weekly.nc')
 
 ####################################################################################
 ################################ Updates ###########################################
---
+++ mloceanbl/models.py
@@ -0,0 +1,188 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from netCDF4 import Dataset
+import seaborn as sns
+import cartopy
+import cartopy.crs as ccrs
+from mpl_toolkits.basemap import maskoceans
+import tensorflow as tf
+
+class sss_sst_ssh:
+    def __init__(self):
+        with xr.open_dataset('sss_sst_ssh_normed_anomalies_weekly.nc') as ds:
+            print(ds)
+            self.sal = ds.norm_salinity
+            self.temp = ds.norm_temperature
+            self.height = ds.norm_height
+            self.lat = ds.lat.values
+            self.lon = ds.lon.values
+            self.time = ds.time.values
+        self.a = 1
+    
+def plot_stats(var, time, lats, lons, text, quantile = 0.5):
+    import matplotlib.gridspec as gridspec
+    # Plot the 'stat' statistic (either 'mean', or 'std') of variable 'var'
+    # Along with temporal cdf's of randomly chosen points
+    # Pass lat and lon values
+
+    assert lats.size == var.shape[1], 'lat dimension does not match'
+    assert lons.size == var.shape[2], 'lon dimension does not match'
+    means = np.nanmean(var, axis=(1, 2))
+    sort_i = np.argsort(means)
+    sort_means = means[sort_i]
+    q = np.quantile(means, quantile)
+    print(q)
+    q_i = np.argwhere(sort_means > q)[0]
+    print(q_i)
+    t_i = sort_i[q_i][0]
+    print(t_i)
+
+    fig = plt.figure(figsize=(18,5), constrained_layout = True)
+    gs = gridspec.GridSpec(nrows=1, ncols=2, figure = fig, width_ratios = [2, 1])
+
+    ax1 = fig.add_subplot(gs[0, :-1], projection=ccrs.Mollweide())
+
+    cs = ax1.contourf(lons, lats, var[t_i], cmap = 'Spectral', transform=ccrs.PlateCarree())
+    plt.colorbar(cs, ax = ax1, aspect=10, fraction = 0.05)
+    ax1.coastlines()
+    ax1.set_aspect('auto', adjustable=None)
+    tt = pd.to_datetime(str(time[t_i])).strftime('%Y.%m.%d')
+    ax1.set_title(str(quantile) + ' Quantile of ' + text + '\n time :' + tt )
+
+    ax2 = fig.add_subplot(gs[0, -1])
+    i = np.random.choice(np.arange(10, lats.size-10), 80)
+    j = np.random.choice(np.arange(10, lons.size-10), 80)
+    for ik, jk in zip(i, j):
+        if np.isnan(var[0, ik, jk]):
+            continue
+        values, base = np.histogram(var[:, ik, jk], bins=40)
+        cumulative = np.cumsum(values)/var.shape[0]
+        ax2.plot(base[:-1], cumulative, c='blue', linewidth=0.2)
+        ax1.scatter(lons[jk], lats[ik], c= 'k', s = 80, marker='*', transform=ccrs.PlateCarree())
+    values, base = np.histogram(np.random.randn(var.shape[0]), bins = 40)
+    cumulative = np.cumsum(values)/var.shape[0]
+    ax2.plot(base[:-1], cumulative, 'k--', linewidth=2.0, label='Normal CDF')
+    ax2.set_title('Temporal CDF of Starred Locations')
+    ax2.grid()
+    ax2.legend()
+    ax2.set_xlim(-3, 3)
+    plt.savefig(text+'_' + str(int(100*quantile)) + '_' + 'quantile.png')
+    plt.show()
+    
+def plot_eof(var, lats, lons, text, n = 2):
+    from sklearn.decomposition import PCA
+    import matplotlib.gridspec as gridspec
+    # Plot leading 'n' EOF's of variable 'var' in one of the two
+    # following axes set up,   limit n to 5      
+    # if n is even:                if n is odd:
+    #    EOF1 EOF2                  EOF1 EOF2
+    #      ...                        ...
+    #    EOF-2 EOF-1                EOF-1 sing. values
+    #     sing. values
+    n = min(n, 5)
+
+    pca = PCA(n_components=15)
+    mask = np.isfinite(var.values.mean(axis=0))
+    pca.fit(var.values[:, mask])
+    v = np.NaN*np.ones_like(var[0])
+
+    fig = plt.figure(figsize = (16,4))
+    
+    gs = gridspec.GridSpec(nrows=n//2, ncols=3, figure = fig, width_ratios = [10, 10, 1])
+    levs = np.linspace(np.nanmin(pca.components_[:n]), np.nanmax(pca.components_[:n]), 15)
+    for j in range(n):
+        row = j // 2
+        col = j % 2
+        axes = fig.add_subplot(gs[row, col], projection=ccrs.Robinson())
+        v[mask] = pca.components_[j]
+        cs = axes.contourf(lons, lats, v, levels = levs, cmap='Spectral', transform=ccrs.PlateCarree())
+#         plt.colorbar(cs, ax = axes)
+        axes.coastlines()
+        axes.set_title('EOF # ' + str(j+1) + ' with explained variance ratio = {:1.2f}'.format(pca.explained_variance_ratio_[j]))
+        axes.set_aspect('auto', adjustable=None)
+    
+    f3_ax4 = fig.add_subplot(gs[:, -1])
+    plt.colorbar(cs, cax = f3_ax4, aspect = 5, fraction = 0.01);
+    plt.suptitle(text)
+    plt.savefig(text+'_'+str(n)+'_'+'EOF.png')
+    plt.show()
+
+def plot_hovmoller(var, time, lats, lons, string):
+    import matplotlib.gridspec as gridspec
+    import metpy.calc as mpcalc
+    import cartopy.feature as cfeature
+    pd.plotting.register_matplotlib_converters()
+    # Start figure
+    ## Make Hovmuller Diagram
+
+    # Compute weights and take weighted average over latitude dimension
+    weights = np.cos(np.deg2rad(lats))
+    avg_data = (var * weights[None, :, None]).sum(axis=1) / np.sum(weights)
+
+    # Get times and make array of datetime objects
+    vtimes = time.astype('datetime64[ms]').astype('O')
+
+    fig = plt.figure(figsize=(15, 20))
+
+    # Use gridspec to help size elements of plot; small top plot and big bottom plot
+    gs = gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[1, 6], hspace=0.03)
+
+    # Tick labels
+    x_tick_labels = [u'180\N{DEGREE SIGN}E', u'90\N{DEGREE SIGN}W',
+                     u'0\N{DEGREE SIGN}E', u'90\N{DEGREE SIGN}E',
+                     u'180\N{DEGREE SIGN}E']
+
+    # Top plot for geographic reference (makes small map)
+    ax1 = fig.add_subplot(gs[0, 0], projection=ccrs.PlateCarree())
+    ax1.set_extent([-180, 180, np.round(lats.min())-5, np.round(lats.max())+5], ccrs.PlateCarree())
+    ax1.set_yticks([np.round(lats.min()), np.round(lats.max())])
+    ax1.set_yticklabels([u'{:2.1f}\N{DEGREE SIGN}S'.format(np.round(lats.min())), u'{:2.1f}\N{DEGREE SIGN}S'.format(np.round(lats.max()))])
+    ax1.set_xticks([-180, -90, 0, 90, 180])
+    ax1.set_xticklabels(x_tick_labels)
+    ax1.grid(linestyle='dotted', linewidth=2)
+
+    # Add geopolitical boundaries for map reference
+    ax1.add_feature(cfeature.COASTLINE.with_scale('50m'))
+    ax1.add_feature(cfeature.LAKES.with_scale('50m'), color='black', linewidths=0.5)
+
+    # Set some titles
+    plt.title('Hovmoller Diagram', loc='left')
+    plt.title(string, loc='right')
+
+    # Bottom plot for Hovmoller diagram
+    ax2 = fig.add_subplot(gs[1, 0])
+    # ax2.invert_yaxis()  # Reverse the time order to do oldest first
+
+    # Plot of chosen variable averaged over latitude and slightly smoothed
+    # clevs = np.arange(-5, 5, 10)
+    cf = ax2.contourf(lons, vtimes, mpcalc.smooth_n_point(
+        avg_data, 9, 2), cmap=plt.cm.bwr, extend='both')
+    cs = ax2.contour(lons, vtimes, mpcalc.smooth_n_point(
+        avg_data, 9, 2), colors='k', linewidths=1)
+    cbar = plt.colorbar(cf, orientation='horizontal', pad=0.04, aspect=50, extendrect=True)
+    cbar.set_label('m $s^{-1}$')
+
+    # Make some ticks and tick labels
+    ax2.set_xticks([-177.5, -90, 0, 90, 177.5])
+    ax2.set_xticklabels(x_tick_labels)
+    ax2.set_yticks(pd.date_range(start=vtimes[0], end=vtimes[-1], freq="3MS").strftime('%Y.%m.%d'))
+    ax2.set_yticklabels(pd.date_range(start=vtimes[0], end=vtimes[-1], freq="3MS").strftime('%Y.%m.%d'))
+
+    # Set some titles
+    plt.title(string, loc='left', fontsize=10)
+    plt.title('Time Range: {0:%Y%m} - {1:%Y%m}'.format(vtimes[0], vtimes[-1]),
+              loc='right', fontsize=10)
+    
+    if lats[0] < 0:
+        min_lat_string = str(int(-lats[0]))+'S'
+    else:
+        min_lat_string = str(int(lats[0]))+'N'
+    if lats[-1] <0:
+        max_lat_string = str(int(-lats[-1]))+'S'
+    else:
+        max_lat_string = str(int(lats[-1]))+'N'
+    plt.savefig(string+'_'+min_lat_string+'_'+max_lat_string+'_'+'hovmoller.png')
+
+    plt.show()
